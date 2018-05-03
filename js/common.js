121d49f1a65df9191cf5e6d409452446c7ab6f0b
yue5454 <yue5454@126.com>
2018-05-03 23:40:21 +0800

    expend2 add

--- js/common.js
+++ js/common.js
@@ -2,4 +2,5 @@ $(function () {
 	var bodyHeight = $(window).height()
 	$('body').height(bodyHeight)
 	$('.about-box').height(bodyHeight-110)
+	$("body").find("#top6").attr('href', '../xf_pc_about.html')
 })
